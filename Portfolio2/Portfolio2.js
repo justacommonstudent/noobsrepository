dfd3c96c25da5cc361a10e8a28b626cda438a936
Ignacio <passionignacio@gmail.com>
2022-08-19 12:11:39 -0300

    new stuff

---
+++ Portfolio2/Portfolio2.js
@@ -0,0 +1,47 @@
+
+
+//first card
+
+  setTimeout(() => {
+    document.getElementById('border');
+    border.style.visibility = 'hidden';
+  }, 3900);
+
+  setTimeout(() => {
+    document.getElementById("border2");
+    border2.style.visibility = "hidden";
+  }, 5000);
+  
+  setTimeout(() => {
+    document.getElementById("cardtext");
+    cardtext.style.visibility = "hidden";
+  }, 7000);
+
+//second card
+
+  setTimeout(() => {
+    document.getElementById("cardtext2");
+    cardtext2.style.visibility = "visible";
+  }, 8000);
+
+  setTimeout(() => {
+    document.getElementById("border4");
+    border4.style.visibility = "hidden";
+  }, 8500);
+
+  setTimeout(() => {
+    document.getElementById("border5");
+    border5.style.visibility = "hidden";
+  }, 9000);
+
+  setTimeout(() => {
+    document.getElementById("border6");
+    border6.style.visibility = "hidden";
+  }, 12000);
+
+  //third card
+
+  setTimeout(() => {
+    document.getElementById("cardtext3");
+    cardtext3.style.visibility = "visible";
+  }, 13000);
